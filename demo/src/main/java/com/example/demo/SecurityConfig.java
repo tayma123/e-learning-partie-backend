6a400f938d77c2e04a5c3287cb12585147a7b60a
tayma ben hmida <taymabenhmida1@gmail.com>
2021-07-16 00:00:31 +0200

    commit 5

--- demo/src/main/java/com/example/demo/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
        http
                .csrf().disable()
                .authorizeRequests()
+               .antMatchers("/registration/**").permitAll()
                  .anyRequest()
                    .authenticated()
                      .and()
