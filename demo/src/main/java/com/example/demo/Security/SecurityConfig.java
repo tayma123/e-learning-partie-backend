d044dcdec030b2d98a2626315edc2f673473978c
tayma ben hmida <taymabenhmida1@gmail.com>
2021-08-03 15:02:50 +0200

    verification mail

--- demo/src/main/java/com/example/demo/Security/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/Security/SecurityConfig.java
@@ -37,7 +37,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
        http
                .csrf().disable()
                .authorizeRequests()
-               .antMatchers("/register/**").permitAll()
+               .antMatchers("/register/**","/confirm-account/**")
+               .permitAll()
+
                  .anyRequest()
                    .authenticated()
                      .and()
